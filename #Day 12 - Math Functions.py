b16afc71bed29a7e19b7a06225aaca8b7b00ab87
akhilamadavaram05 <akhilamadavaram05@gmail.com>
2026-04-16 21:13:18 +0530

    Add files via upload

---
+++ #Day 12 - Math Functions.py
@@ -0,0 +1,9 @@
+#Day 12 - Math Functions
+def square(number):
+    return number * number
+
+def cube(number):
+    return number * number * number
+
+print("5 squared:", square(5))
+print("3 cubed:", cube(3))
