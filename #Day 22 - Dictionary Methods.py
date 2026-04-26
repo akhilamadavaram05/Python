4c8810ef44a35de0087eab914b4eeac0dce1a02c
akhilamadavaram05 <akhilamadavaram05@gmail.com>
2026-04-26 09:00:46 +0530

    Add files via upload

---
+++ #Day 22 - Dictionary Methods.py
@@ -0,0 +1,11 @@
+#Day 22 - Dictionary Methods
+person = {"name": "Sai", "age": 25}
+
+# keys() method
+print("Keys:", list(person.keys()))
+
+# values() method
+print("Values:", list(person.values()))
+
+# items() method
+print("Items:", list(person.items()))
